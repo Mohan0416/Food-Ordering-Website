05bd8c15849fc076eb43026063311114cafdb9bf
Mohan0416 <manomohan417@gmail.com>
2025-02-20 21:19:30 +0530

    header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="content">
+        <h2>Order your Favorite food here</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo possimus dolor provident suscipit voluptas praesentium assumenda dolores</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
