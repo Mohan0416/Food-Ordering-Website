1335352faaff305f4c207495b8f4d5dc9c64f9ad
Mohan0416 <manomohan417@gmail.com>
2025-03-11 21:36:29 +0530

    login page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Cart from './pages/Cart/Cart.jsx'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import Login from './components/Login/Login.jsx'
 
 const App = () => {
+  const[showLogin,setShowLogin] = useState(false)
+
   return (
     <>
+    {showLogin?<Login/>:<></>}
     <div className='app'>
-      <Navbar />
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/cart' element={<Cart/>} />
